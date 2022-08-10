d4c6f26ce9d922681604b7aff310562c42c76ed3
MagomedIbr <magomedibragimov@yahoo.de>
2022-08-10 18:34:38 +0200

    text Feld um die Farbe zu ändern

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const doc = document
 const button = doc.getElementById("mybutton")
 const inputText = doc.getElementById("input")
+const form = doc.getElementById("form")
+cc = inputText.value;
 // function changeBg(e){
 // doc.body.background = e;
 // console.log("cum")
 // }
 const color = ['green','red','blue','black','yellow','pink','purple','orange','white','grey','lime','lightgreen','tomato','turqoise','aquamarine','brown','coral','chocolate','crimson','cyan','gold','fuchsia','violet','turquoise']
+
 
 button.addEventListener("click", function(){
     const a = doc.body.style.background;
@@ -13,12 +16,11 @@ button.addEventListener("click", function(){
     doc.body.style.background = randomColor;
     alert("Your Color wil be changed to " + randomColor);
     console.log(randomColor ); });
-    
-input.addEventListener("click", function () {
-    const a = doc.body.style.background;
-    const randomColor = color[Math.floor(Math.random() * color.length)]; //https://www.delftstack.com/howto/javascript/select-a-random-element-from-an-array-in-javascript/
-    doc.body.style.background = randomColor;
-    alert("Your Color wil be changed to " + randomColor);
-    console.log(randomColor);
-});
+
+form.addEventListener("submit", function(e) {
+e.preventDefault();
+alert(inputText.value)
+doc.body.style.background = inputText.value;
+})
+
 
