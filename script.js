8fde16613f1ed87a2dc830083fecaa0c34ad32b4
MagomedIbr <magomedibragimov@yahoo.de>
2022-08-11 14:43:29 +0200

    alles in arrow Methoden übermittelt

--- script.js
+++ script.js
@@ -10,20 +10,21 @@ cc = inputText.value;
 const color = ['green','red','blue','black','yellow','pink','purple','orange','white','grey','lime','lightgreen','tomato','turqoise','aquamarine','brown','coral','chocolate','crimson','cyan','gold','fuchsia','violet','turquoise']
 
 
-button.addEventListener("click", function(){
+button.addEventListener("click", ()=>{
     const a = doc.body.style.background;
     const randomColor = color[Math.floor(Math.random() * color.length)]; //https://www.delftstack.com/howto/javascript/select-a-random-element-from-an-array-in-javascript/
     doc.body.style.background = randomColor;
     alert("Your Color wil be changed to " + randomColor);
-    console.log(randomColor ); });
+    console.log(randomColor); 
+});
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", (e) => {
 e.preventDefault();
 if(inputText.value=="random"){
     const a = doc.body.style.background;
     const randomColor = color[Math.floor(Math.random() * color.length)]; //https://www.delftstack.com/howto/javascript/select-a-random-element-from-an-array-in-javascript/
     doc.body.style.background = randomColor;
-    alert("Your Color wil be changed to a random color" );
+    alert("Your Color wil be changed to a random color");
 }else{
 alert("Your Color wil be changed to "+inputText.value)
 doc.body.style.background = inputText.value;
